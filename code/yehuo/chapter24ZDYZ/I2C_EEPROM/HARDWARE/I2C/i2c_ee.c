1638eff9c0cbf40abea7e5ab190bb86725244a91
ahwi <jyws66@qq.com>
2023-12-28 11:12:30 +0800

    更新代码: 24章 I2C-读写EEPROM

--- code/yehuo/chapter24ZDYZ/I2C_EEPROM/HARDWARE/I2C/i2c_ee.c
+++ code/yehuo/chapter24ZDYZ/I2C_EEPROM/HARDWARE/I2C/i2c_ee.c
@@ -36,7 +36,7 @@ void I2C_GPIO_Config(void)
 	GPIO_Init(GPIOB, &GPIO_InitStruct);
 	
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);
-		GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
 }
 
 /**
@@ -150,7 +150,7 @@ uint8_t EEPROM_Page_Write(uint8_t addr, uint8_t *data, uint8_t size)
 	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
 		count_wait --;
 		if(count_wait == 0){
-			return Error_CallBack(1);
+			return Error_CallBack(4);
 		}
 	}
 	
@@ -164,7 +164,7 @@ uint8_t EEPROM_Page_Write(uint8_t addr, uint8_t *data, uint8_t size)
 	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) != SUCCESS){
 		count_wait --;
 		if(count_wait == 0){
-			return Error_CallBack(2);
+			return Error_CallBack(5);
 		}
 	}
 	
@@ -177,7 +177,7 @@ uint8_t EEPROM_Page_Write(uint8_t addr, uint8_t *data, uint8_t size)
 	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED) != SUCCESS){
 		count_wait --;
 		if(count_wait == 0){
-			return Error_CallBack(3);
+			return Error_CallBack(6);
 		}
 	}
 	
@@ -191,7 +191,7 @@ uint8_t EEPROM_Page_Write(uint8_t addr, uint8_t *data, uint8_t size)
 		while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED) != SUCCESS){
 			count_wait --;
 			if(count_wait == 0){
-				return Error_CallBack(3);
+				return Error_CallBack(7);
 			}
 		}
 		data ++;
@@ -213,7 +213,236 @@ uint8_t EEPROM_Page_Write(uint8_t addr, uint8_t *data, uint8_t size)
 */
 uint8_t EEPROM_Buffer_Write(uint8_t addr, uint8_t *data, uint16_t size)
 {
+	// 页对齐，计算出第一页要写的大小 firstSize
+	uint8_t remainder1 = addr % EEPROM_PAGE_SIZE;
+	uint8_t firstSize = EEPROM_PAGE_SIZE - remainder1;
+	
+	// 扣除第一页写的大小，剩余要写的数据
+	uint8_t remainSize = size - firstSize;
+	uint8_t remainder2 = remainSize % EEPROM_PAGE_SIZE;
+	// 剩余要写的页大小
+	uint8_t remainPage = remainSize / EEPROM_PAGE_SIZE;
+	
+	if(firstSize == (uint8_t)(EEPROM_PAGE_SIZE)){ //第一页刚好是对齐的
+		remainPage += 1; // 把第一页也加上去
 
+	} 
+	else{ // 未对齐，先把第一页的数据写上
+		EEPROM_Page_Write(addr, data, firstSize);
+		Wait_For_Standby();
+		addr += firstSize;
+		data += firstSize;
+	}
+
+	while(remainPage -- ){ // 一页一页写
+		EEPROM_Page_Write(addr, data, EEPROM_PAGE_SIZE);
+		Wait_For_Standby();
+		addr += EEPROM_PAGE_SIZE;
+		data += EEPROM_PAGE_SIZE;
+	}
+		
+	// 写剩余的数据
+	if(remainder2 != 0){
+		EEPROM_Page_Write(addr, data, remainder2);
+		Wait_For_Standby();
+		addr += remainder2;
+		data += remainder2;
+	}
+	
+	return 0;
+}
+
+/**
+* @brief 从EEPROM读取一个字节数据
+* @param addr: 要读取的地址
+* @param data: 保存读取的数据
+* @return 0 成功; 非0 失败
+*/
+uint8_t EEPROM_Random_Read(uint8_t addr, uint8_t *data)
+{
+	// 起始信号
+	I2C_GenerateSTART(I2C1, ENABLE);
+	
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	
+	// 等待EV5事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(9);
+		}
+	}
+	
+	// 发送要读取的I2C设备地址，并设置为写方向
+	I2C_Send7bitAddress(I2C1, (uint8_t)(EEPROM_ADDR), I2C_Direction_Transmitter);
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV6事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(10);
+		}
+	}
+	
+	// 发送要读取的EEPROM数据地址
+	I2C_SendData(I2C1, addr);
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV8_2事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(11);
+		}
+	}
+	
+	
+	/********** 重新发送起始信号，开始读取数据 ***************/
+	// 起始信号
+	I2C_GenerateSTART(I2C1, DISABLE);
+	
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV5事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(12);
+		}
+	}
+	
+	// 发送要读取的I2C设备地址，并设置为写方向
+	I2C_Send7bitAddress(I2C1, (uint8_t)(EEPROM_ADDR), I2C_Direction_Receiver);
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV6事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(13);
+		}
+	}
+	
+	// 做出非应答信号(在接收数据前配置好)
+	I2C_AcknowledgeConfig(I2C1, ENABLE);
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV7事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(14);
+		}
+	}
+	
+	// 读取数据
+	*data = I2C_ReceiveData(I2C1);
+	
+	// 产生停止信号
+	I2C_GenerateSTOP(I2C1, ENABLE);
+	return 0;
+}
+
+/**
+* @brief 从EEPROM连续读取数据
+* @param addr: 要读取的数据单元格地址
+* @param data: 保存读取的数据
+* @param size: 要读取多少个数据
+* @return 0 成功; 非0 失败
+*/
+uint8_t EEPROM_Buffer_Read(uint8_t addr, uint8_t *data, uint16_t size)
+{
+		// 起始信号
+	I2C_GenerateSTART(I2C1, ENABLE);
+	
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	
+	// 等待EV5事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(9);
+		}
+	}
+	
+	// 发送要读取的I2C设备地址，并设置为写方向
+	I2C_Send7bitAddress(I2C1, (uint8_t)(EEPROM_ADDR), I2C_Direction_Transmitter);
+	
+	// 等待EV6事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(10);
+		}
+	}
+	
+	// 发送要读取的EEPROM数据地址
+	I2C_SendData(I2C1, addr);
+	// 等待EV8_2事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(11);
+		}
+	}
+	
+	
+	/********** 重新发送起始信号，开始读取数据 ***************/
+	// 起始信号
+	I2C_GenerateSTART(I2C1, ENABLE);
+	
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV5事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(12);
+		}
+	}
+	
+	// 发送要读取的I2C设备地址，并设置为写方向
+	I2C_Send7bitAddress(I2C1, (uint8_t)(EEPROM_ADDR), I2C_Direction_Receiver);
+	// 重置 count_wait
+	count_wait = TIME_OUT;
+	// 等待EV6事件，直到检测成功
+	while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED) != SUCCESS){
+		count_wait --;
+		if(count_wait == 0){
+			return Error_CallBack(13);
+		}
+	}
+	
+	// 读取数据
+	while(size --){
+		if(size == 0){
+			// 做出非应答信号(在接收数据前配置好)
+			I2C_AcknowledgeConfig(I2C1, DISABLE);
+		} else {
+			// 做出应答信号(在接收数据前配置好)
+			I2C_AcknowledgeConfig(I2C1, ENABLE);
+		}
+
+		// 重置 count_wait
+		count_wait = TIME_OUT;
+		// 等待EV7事件，直到检测成功
+		while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED) != SUCCESS){
+			count_wait --;
+			if(count_wait == 0){
+				return Error_CallBack(14);
+			}
+		}
+		
+		*data++ = I2C_ReceiveData(I2C1);
+	}
+	
+	
+	// 产生停止信号
+	I2C_GenerateSTOP(I2C1, ENABLE);
+	return 0;
 }
 
 /**
@@ -231,7 +460,7 @@ uint8_t Wait_For_Standby(void)
 		while(I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS){
 			count_wait --;
 			if(count_wait == 0){
-				return Error_CallBack(11);
+				return Error_CallBack(8);
 			}
 		}
 		
@@ -254,7 +483,6 @@ uint8_t Wait_For_Standby(void)
 	I2C_GenerateSTOP(I2C1, ENABLE);
 	return 1;
 }
-
 
 uint8_t Error_CallBack(uint8_t code)
 {
--- code/yehuo/chapter24ZDYZ/I2C_EEPROM/HARDWARE/I2C/i2c_ee.h
+++ code/yehuo/chapter24ZDYZ/I2C_EEPROM/HARDWARE/I2C/i2c_ee.h
@@ -4,7 +4,8 @@
 #include "stm32f4xx.h"
 
 #define I2C_SPEED 400000
-#define I2C_ADDR  0xA0
-#define EEPROM_ADDR 0x50
+#define I2C_ADDR  0x01
+#define EEPROM_ADDR 0xA0
+#define EEPROM_PAGE_SIZE 8
 
 #endif
